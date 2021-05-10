b1a4ac6d2f879e0386d06758f75fd5d8f03818ad
sc-nick <nick.andrianaharimalala@sc-mauritius.com>
2021-05-10 10:33:18 +0300

    Demo added

---
+++ demo/src/app/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { SafePipe } from '../utils/SafePipe';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, SafePipe],
+})
+export class HomePageModule {}
---
+++ demo/src/app/home/home.page.ts
@@ -0,0 +1,80 @@
+import { Component, NgZone } from '@angular/core';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+
+declare var window;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  images: Array<any> = [];
+  grid: Array<Array<string>> = [];
+  imgPerRow: number = 3;
+  options: any;
+
+  constructor(private zone: NgZone, private webview: WebView) {
+    this.requestReadPermission();
+  }
+
+  openGallery() {
+    this.options = {
+      outputType: 1,
+    };
+
+    window.imagePicker.getPictures(
+      (results) => {
+        this.images = [];
+        this.images = [];
+
+        this.zone.run(() => {
+          for (var i = 0; i < results.length; i++) {
+            let file = 'file://' + results[i];
+            let path = this.webview.convertFileSrc(file);
+            this.images.push(path);
+          }
+          this.splitGrid(this.imgPerRow);
+        });
+      },
+      (error) => {
+        console.log('Error: ' + error);
+      },
+      this.options
+    );
+  }
+
+  hasReadPermission() {
+    window.imagePicker.hasReadPermission(function (result) {
+      if (!result) {
+        this.requestReadPermission();
+      }
+    });
+  }
+
+  requestReadPermission() {
+    window.imagePicker.requestReadPermission();
+  }
+
+  splitGrid(perRow: number) {
+    let row = 0;
+    this.grid = Array(Math.ceil(this.images.length / perRow));
+
+    if (this.images.length == 0) {
+      this.grid = [];
+    }
+
+    for (let i = 0; i < this.images.length; i += perRow) {
+      this.grid[row] = Array(perRow);
+      for (let j = 0; j < perRow; j++) {
+        this.images[i + j] ? (this.grid[row][j] = this.images[i + j]) : '';
+      }
+      row++;
+    }
+  }
+
+  removeImage(image: string) {
+    this.images = this.images.filter((img) => img !== image);
+    this.splitGrid(this.imgPerRow);
+  }
+}
