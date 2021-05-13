d13619c455b25b82f4331853829607dbac7528e5
Azhar Beebeejaun <azhar.beebeejaun@sc-mauritius.com>
2021-05-13 11:56:36 +0400

    Update ImagePicker.java

--- src/android/ImagePicker.java
+++ src/android/ImagePicker.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
